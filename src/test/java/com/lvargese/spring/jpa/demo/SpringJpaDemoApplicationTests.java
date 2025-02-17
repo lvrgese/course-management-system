7640d2a086954cc3c7f00f362fd04750ee402717
Liyons Varghese <liyonsvarghese@gmail.com>
2025-02-17 11:00:20 +0530

    Initial commit

---
+++ src/test/java/com/lvargese/spring/jpa/demo/SpringJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvargese.spring.jpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
