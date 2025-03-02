9c3948c8a971a2a94cc85e0b0b81c1c4f7cfd5be
Liyons Varghese <liyonsvarghese@gmail.com>
2025-03-02 11:51:44 +0530

    TeacherServiceSkeleton

--- src/main/java/com/lvargese/courseapi/service/TeacherService.java
+++ src/main/java/com/lvargese/courseapi/service/TeacherService.java
@@ -1,4 +1,5 @@
 package com.lvargese.courseapi.service;
+import com.lvargese.courseapi.dto.CourseDTO;
 import com.lvargese.courseapi.dto.TeacherDTO;
 import com.lvargese.courseapi.entity.Course;
 import com.lvargese.courseapi.entity.Teacher;
@@ -7,9 +8,10 @@ import com.lvargese.courseapi.exception.ResourceNotFoundException;
 import java.util.List;
 
 public interface TeacherService {
-    TeacherDTO createTeacher(Teacher teacher);
+    TeacherDTO createTeacher(TeacherDTO teacherDTO);
     TeacherDTO getTeacherById(Long id) throws ResourceNotFoundException;
-    List<Teacher> getAllTeachers();
-    List<Course> getCoursesByTeacher(Long teacherId);
-    TeacherDTO deleteTeacher(Long teacherId);
+    List<TeacherDTO> getAllTeachers();
+    TeacherDTO updateTeacher(Long teacherId, TeacherDTO teacherDTO) throws ResourceNotFoundException;
+    void deleteTeacher(Long teacherId) throws ResourceNotFoundException;
+    List<CourseDTO> getCoursesByTeacher(Long teacherId) throws ResourceNotFoundException;
 }
--- src/main/java/com/lvargese/courseapi/service/TeacherServiceImpl.java
+++ src/main/java/com/lvargese/courseapi/service/TeacherServiceImpl.java
@@ -1,8 +1,7 @@
 package com.lvargese.courseapi.service;
 
+import com.lvargese.courseapi.dto.CourseDTO;
 import com.lvargese.courseapi.dto.TeacherDTO;
-import com.lvargese.courseapi.entity.Course;
-import com.lvargese.courseapi.entity.Teacher;
 import com.lvargese.courseapi.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +9,9 @@ import java.util.List;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
+
     @Override
-    public TeacherDTO createTeacher(Teacher teacher) {
+    public TeacherDTO createTeacher(TeacherDTO teacherDTO) {
         return null;
     }
 
@@ -21,17 +21,22 @@ public class TeacherServiceImpl implements TeacherService {
     }
 
     @Override
-    public List<Teacher> getAllTeachers() {
+    public List<TeacherDTO> getAllTeachers() {
         return List.of();
     }
 
     @Override
-    public List<Course> getCoursesByTeacher(Long teacherId) {
-        return List.of();
-    }
-
-    @Override
-    public TeacherDTO deleteTeacher(Long teacherId) {
+    public TeacherDTO updateTeacher(Long teacherId, TeacherDTO teacherDTO) throws ResourceNotFoundException {
         return null;
+    }
+
+    @Override
+    public void deleteTeacher(Long teacherId) throws ResourceNotFoundException {
+
+    }
+
+    @Override
+    public List<CourseDTO> getCoursesByTeacher(Long teacherId) throws ResourceNotFoundException {
+        return List.of();
     }
 }
